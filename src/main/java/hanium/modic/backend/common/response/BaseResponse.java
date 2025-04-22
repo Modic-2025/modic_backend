c4905dca2fb0c4c1338ca79a8168ae8dedab5fce
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-04-22 16:21:57 +0900

    fix: BaseResponse isSuccess 필드 오류 수정
    - Json으로 변환되면서 "is" prefix가 없어지는 문제 해결

--- src/main/java/hanium/modic/backend/common/response/BaseResponse.java
+++ src/main/java/hanium/modic/backend/common/response/BaseResponse.java
@@ -2,11 +2,15 @@ package hanium.modic.backend.common.response;
 
 import org.springframework.http.HttpStatusCode;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 
 @Getter
 public abstract class BaseResponse {
-	private final boolean isSuccess;
+
+	@JsonProperty("isSuccess")
+	private final Boolean isSuccess;
 	private final int status;
 
 	protected BaseResponse(boolean isSuccess, HttpStatusCode status) {
