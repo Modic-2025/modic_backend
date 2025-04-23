2966db53030d3894c56e926573bd9ded226f6d44
youth <boysoeng@naver.com>
2025-04-23 13:59:36 +0900

    feat: post 기준 이미지 조회 메서드 추가

--- src/main/java/hanium/modic/backend/domain/post/repository/PostImageEntityRepository.java
+++ src/main/java/hanium/modic/backend/domain/post/repository/PostImageEntityRepository.java
@@ -1,7 +1,10 @@
 package hanium.modic.backend.domain.post.repository;
 
+import hanium.modic.backend.domain.post.entity.PostEntity;
 import hanium.modic.backend.domain.post.entity.PostImageEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostImageEntityRepository extends JpaRepository<PostImageEntity, Long> {
+    List<PostImageEntity> findByPostEntity(PostEntity postEntity);
 }
