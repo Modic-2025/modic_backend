4fd4b57e4ff0220c85eb38497f37d77292586695
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2025-04-25 23:11:48 +0900

    feat: 파라미터 검증 조건 추가

--- src/main/java/hanium/modic/backend/web/controller/PostController.java
+++ src/main/java/hanium/modic/backend/web/controller/PostController.java
@@ -4,17 +4,20 @@ import hanium.modic.backend.common.response.ApiResponse;
 import hanium.modic.backend.domain.post.service.PostService;
 import hanium.modic.backend.web.dto.CreatePostRequest;
 import hanium.modic.backend.web.dto.GetPostResponse;
-import hanium.modic.backend.web.dto.GetPostsRequest;
 import hanium.modic.backend.web.dto.PageResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @RequestMapping("/api/posts")
+@Validated
 public class PostController {
 
     private final PostService postService;
@@ -33,12 +36,13 @@ public class PostController {
 
     @GetMapping
     public ResponseEntity<ApiResponse<PageResponse<GetPostResponse>>> getPosts(
-            @RequestParam(required = false) String sort,
-            @RequestParam(required = false) Integer page,
-            @RequestParam(required = false) Integer size
+            @RequestParam(required = false, defaultValue = "LATEST") String sort,
+            @RequestParam(required = false, defaultValue = "0") @Min(value = 0, message = "페이지 번호는 0 이상이어야 합니다") Integer page,
+            @RequestParam(required = false, defaultValue = "10")
+            @Min(value = 10, message = "페이지 크기는 10 이상이어야 합니다.")
+            @Max(value = 20, message = "페이지 크기는 20 이하여야 합니다.") Integer size
     ) {
-        GetPostsRequest request = GetPostsRequest.of(sort, page, size);
-        PageResponse<GetPostResponse> response = postService.getPosts(request.sort(), request.page(), request.size());
+        PageResponse<GetPostResponse> response = postService.getPosts(sort, page, size);
         return ResponseEntity.ok(ApiResponse.ok(response));
     }
 }
