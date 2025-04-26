b3f2320e11d2ebabb6bbca5fd53bf6a52a17f0f0
yooooonshine <hl3470@naver.com>
2025-04-26 17:06:24 +0900

    feat: swagger 도입

---
+++ src/main/java/hanium/modic/backend/common/property/property/SwaggerProperties.java
@@ -0,0 +1,15 @@
+package hanium.modic.backend.common.property.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "swagger")
+public class SwaggerProperties {
+	private String url;
+}
---
+++ src/main/java/hanium/modic/backend/common/swagger/conf/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package hanium.modic.backend.common.swagger.conf;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hanium.modic.backend.common.property.property.SwaggerProperties;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+
+	private final SwaggerProperties swaggerProperties;
+
+	@Bean
+	public OpenAPI openAPI() {
+
+		SecurityScheme securityScheme = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER).name("Authorization");
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+		return new OpenAPI()
+			.servers(List.of(new Server().url(swaggerProperties.getUrl()).description("백엔드 서버")))
+			.components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+			.security(Arrays.asList(securityRequirement))
+			.info(apiInfo());
+	}
+
+	private Info apiInfo() {
+		return new Info()
+			.title("Modic api 명세서")
+			.description("Modic	 api 명세서입니다.")
+			.version("1.0.0");
+	}
+}
