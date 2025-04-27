40e29bc987bf7e0080023efe798440764a570ac8
yooooonshine <hl3470@naver.com>
2025-04-27 14:52:35 +0900

    feat: S3ImageUtil에 imagePath validation 추가
    - 이름 null or 빈문자열 체크

--- src/main/java/hanium/modic/backend/common/error/ErrorCode.java
+++ src/main/java/hanium/modic/backend/common/error/ErrorCode.java
@@ -21,9 +21,11 @@ public enum ErrorCode {
 	IMAGE_NOT_STORE_EXCEPTION(HttpStatus.BAD_REQUEST, "I-001", "이미지가 저장되지 않았습니다."),
 	IMAGE_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "I-002", "해당 이미지를 찾을 수 없습니다."),
 	INVALID_IMAGE_FILE_NAME_EXCEPTION(HttpStatus.BAD_REQUEST, "I-003", "잘못된 이미지 파일 이름입니다."),
+	INVALID_IMAGE_FILE_PATH_EXCEPTION(HttpStatus.BAD_REQUEST, "I-004", "잘못된 이미지 파일 경로입니다."),
 
 	// Server
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S-001", "서버 내부에서 에러가 발생하였습니다."),
+	S3_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S-002", "S3 서버에서 에러가 발생하였습니다."),
 	;
 
 	private final HttpStatus status;
--- src/main/java/hanium/modic/backend/domain/image/util/S3ImageUtil.java
+++ src/main/java/hanium/modic/backend/domain/image/util/S3ImageUtil.java
@@ -1,6 +1,7 @@
 package hanium.modic.backend.domain.image.util;
 
 import static com.amazonaws.HttpMethod.*;
+import static hanium.modic.backend.common.error.ErrorCode.*;
 
 import java.net.URL;
 import java.util.Date;
@@ -12,11 +13,14 @@ import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 
+import hanium.modic.backend.common.error.exception.AppException;
 import hanium.modic.backend.common.property.property.S3Properties;
 import hanium.modic.backend.domain.image.domain.ImagePrefix;
 import hanium.modic.backend.domain.image.dto.CreateImageSaveUrlDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class S3ImageUtil implements ImageUtil {
@@ -29,19 +33,22 @@ public class S3ImageUtil implements ImageUtil {
 	// 이미지 삭제
 	@Override
 	public void deleteImage(String imagePath) {
+		validateImagePath(imagePath);
+
 		amazonS3Client.deleteObject(s3Properties.getBucketName(), imagePath);
 	}
 
 	// 이미지 URL 생성
 	@Override
 	public String createImageUrl(ImagePrefix imagePrefix, String imagePath) {
+		validateImagePath(imagePath);
+
 		return amazonS3Client.getUrl(s3Properties.getBucketName(), imagePath).toString();
 	}
 
 	// 저장 PreSignedUrl 생성
 	@Override
 	public CreateImageSaveUrlDto createImageSaveUrl(ImagePrefix imagePrefix, String fullFileName) {
-
 		String path = createPath(imagePrefix, fullFileName);
 
 		GeneratePresignedUrlRequest request = createGeneratePreSignedUrlRequest(path, PUT, getUrlExpiration());
@@ -53,6 +60,8 @@ public class S3ImageUtil implements ImageUtil {
 	// 조회 PreSignedUrl 생성
 	@Override
 	public String createImageGetUrl(String imagePath) {
+		validateImagePath(imagePath);
+
 		GeneratePresignedUrlRequest request = createGeneratePreSignedUrlRequest(imagePath, GET, getUrlExpiration());
 		return amazonS3Client.generatePresignedUrl(request).toString();
 	}
@@ -60,6 +69,8 @@ public class S3ImageUtil implements ImageUtil {
 	// 이미지 저장확인
 	@Override
 	public boolean isImageSaved(ImagePrefix imagePrefix, String imagePath) {
+		validateImagePath(imagePath);
+
 		return amazonS3Client.doesObjectExist(s3Properties.getBucketName(), imagePath);
 	}
 
@@ -87,5 +98,12 @@ public class S3ImageUtil implements ImageUtil {
 		expTimeMillis += EXPIRATION_TIME;
 		expiration.setTime(expTimeMillis);
 		return expiration;
+	}
+
+	// imagePath 검사
+	private void validateImagePath(String imagePath) {
+		if (imagePath == null || imagePath.isEmpty()) {
+			throw new AppException(INVALID_IMAGE_FILE_PATH_EXCEPTION);
+		}
 	}
 }
