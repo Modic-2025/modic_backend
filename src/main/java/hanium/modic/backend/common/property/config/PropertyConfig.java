a10b51bd8be1880b6b5035dd5016f3815fb3d301
yooooonshine <hl3470@naver.com>
2025-04-27 13:52:41 +0900

    fix: PropertyConfig에 SwaggerProperties 추가

--- src/main/java/hanium/modic/backend/common/property/config/PropertyConfig.java
+++ src/main/java/hanium/modic/backend/common/property/config/PropertyConfig.java
@@ -4,11 +4,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 import hanium.modic.backend.common.property.property.S3Properties;
+import hanium.modic.backend.common.property.property.SwaggerProperties;
 
 // 전역적으로 사용되는 상수
 @Configuration
 @EnableConfigurationProperties(value = {
-	S3Properties.class
+	S3Properties.class,
+	SwaggerProperties.class
 })
 public class PropertyConfig {
 }
