b67acae9994ffd509cdfc35896d4f4bce23ad7f0
youth <boysoeng@naver.com>
2025-06-01 02:56:52 +0900

    test: login controller

--- src/main/java/hanium/modic/backend/web/auth/dto/LoginRequest.java
+++ src/main/java/hanium/modic/backend/web/auth/dto/LoginRequest.java
@@ -2,9 +2,11 @@ package hanium.modic.backend.web.auth.dto;
 
 import hanium.modic.backend.common.validator.Password;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
 	@Email(message = "유효하지 않은 이메일 형식입니다.")
+	@NotBlank(message = "이메일은 필수 입력 항목입니다.")
 	String email,
 	@Password(message = "비밀번호는 8자 이상 20자 이하, 영문, 숫자, 특수문자를 포함해야 합니다.")
 	String password
