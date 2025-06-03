737d350996d481ce1b37739e441ab229dd066da6
youth <boysoeng@naver.com>
2025-06-04 03:05:28 +0900

    feat: user principal 구현

---
+++ src/main/java/hanium/modic/backend/common/security/principal/UserPrincipal.java
@@ -0,0 +1,55 @@
+package hanium.modic.backend.common.security.principal;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import hanium.modic.backend.domain.user.entity.UserEntity;
+import lombok.Data;
+
+@Data
+public class UserPrincipal implements UserDetails {
+
+	private final UserEntity user;
+
+	public UserPrincipal(UserEntity user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
