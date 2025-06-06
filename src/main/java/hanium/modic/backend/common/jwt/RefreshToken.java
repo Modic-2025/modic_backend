c6ea34f17d7ddc27a9a6d4d894130b08f5d8c6ff
youth <boysoeng@naver.com>
2025-06-07 01:33:10 +0900

    feat: 발급된 refresh token 레디스에 저장

---
+++ src/main/java/hanium/modic/backend/common/jwt/RefreshToken.java
@@ -0,0 +1,26 @@
+package hanium.modic.backend.common.jwt;
+
+import org.springframework.data.redis.core.RedisHash;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24 * 14)
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+
+	@Id
+	private Long userId;
+
+	private String refreshToken;
+
+	public void updateRefreshToken(final String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+}
