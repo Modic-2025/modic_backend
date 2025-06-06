b141c555f59c7dc4dc5480754ca5009e489d116b
youth <boysoeng@naver.com>
2025-06-07 01:14:14 +0900

    feat: 로그인 시 발급된 리프레시 토큰 저장

--- src/main/java/hanium/modic/backend/domain/auth/service/AuthService.java
+++ src/main/java/hanium/modic/backend/domain/auth/service/AuthService.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Service;
 
 import hanium.modic.backend.common.error.ErrorCode;
 import hanium.modic.backend.common.error.exception.AppException;
+import hanium.modic.backend.common.jwt.BlackListRepository;
 import hanium.modic.backend.common.jwt.JwtTokenProvider;
+import hanium.modic.backend.common.jwt.RefreshToken;
+import hanium.modic.backend.common.jwt.RefreshTokenRepository;
 import hanium.modic.backend.domain.auth.dto.Token;
 import hanium.modic.backend.domain.user.entity.UserEntity;
 import hanium.modic.backend.domain.user.repository.UserEntityRepository;
@@ -22,6 +25,10 @@ public class AuthService {
 
 	private final JwtTokenProvider jwtTokenProvider;
 
+	private final RefreshTokenRepository refreshTokenRepository;
+
+	private final BlackListRepository blackListRepository;
+
 	public LoginResponse login(final String email, final String password) {
 		UserEntity user = userEntityRepository.findByEmail(email)
 			.orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND_EXCEPTION));
@@ -31,6 +38,9 @@ public class AuthService {
 		}
 
 		Token token = jwtTokenProvider.createToken(user);
+
+		RefreshToken refreshToken = RefreshToken.builder().userId(user.getId()).refreshToken(token.refreshToken()).build();
+		refreshTokenRepository.save(refreshToken);
 
 		return LoginResponse.from(token);
 	}
