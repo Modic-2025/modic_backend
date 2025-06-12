5660682d3f7375a535b201d90551076f394346e8
youth <boysoeng@naver.com>
2025-06-13 00:20:51 +0900

    test: 회원 가입 시 이메일 인증 코드 발송 API 컨트롤러 테스트

--- src/main/java/hanium/modic/backend/web/auth/dto/SendEmailRequest.java
+++ src/main/java/hanium/modic/backend/web/auth/dto/SendEmailRequest.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record SendEmailRequest(
-	@Email(message = "이메일 형식이 아닙니다.")
-	@NotBlank(message = "값을 입력해주세요.")
+	@Email(message = "유효하지 않은 이메일 형식입니다.")
+	@NotBlank(message = "이메일은 필수 입력 항목입니다.")
 	String email
 ) {
 }
