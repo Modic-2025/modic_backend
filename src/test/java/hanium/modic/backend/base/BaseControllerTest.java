3b0441a8b411cdccfc81cfde1025a2fd515c9d38
yooooonshine <hl3470@naver.com>
2025-06-18 17:02:32 +0900

    feat: BaseControllerTest에 SecurityProperties 내용 추가

--- src/test/java/hanium/modic/backend/base/BaseControllerTest.java
+++ src/test/java/hanium/modic/backend/base/BaseControllerTest.java
@@ -4,11 +4,15 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import hanium.modic.backend.common.jwt.JwtAuthenticationEntryPoint;
 import hanium.modic.backend.common.jwt.JwtAuthenticationFilter;
+import hanium.modic.backend.common.property.property.SecurityProperties;
 
 public class BaseControllerTest {
 
 	@MockitoBean
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	@MockitoBean
+	private SecurityProperties securityProperties;
 
 	@MockitoBean
 	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
