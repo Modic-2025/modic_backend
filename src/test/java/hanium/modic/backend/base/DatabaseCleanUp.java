6cee92b1bd237e7a9b60e1973cba8fbd54d9607e
yooooonshine <hl3470@naver.com>
2025-06-23 10:16:28 +0900

    refactor: DatabaseCleanUp의 불필요한 외래키 제약 조건 수정

--- src/test/java/hanium/modic/backend/base/DatabaseCleanUp.java
+++ src/test/java/hanium/modic/backend/base/DatabaseCleanUp.java
@@ -40,13 +40,10 @@ public class DatabaseCleanUp implements InitializingBean {
 	@Transactional
 	public void execute() {
 		entityManager.flush();
-		entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
 
 		for (String tableName : tableNames) {
 			entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
 			entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
 		}
-
-		entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
 	}
 }
