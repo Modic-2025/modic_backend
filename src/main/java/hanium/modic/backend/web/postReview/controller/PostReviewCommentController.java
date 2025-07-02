8c225ce3deeb5d93c5805d97e48c411495c337cf
yooooonshine <hl3470@naver.com>
2025-07-02 10:56:21 +0900

    refactor: 시큐리티 변경사항에 따라 @AuthenticationPricipal -> @CurrentUser로 변경

--- src/main/java/hanium/modic/backend/web/postReview/controller/PostReviewCommentController.java
+++ src/main/java/hanium/modic/backend/web/postReview/controller/PostReviewCommentController.java
@@ -1,5 +1,18 @@
 package hanium.modic.backend.web.postReview.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import hanium.modic.backend.common.annotation.user.CurrentUser;
 import hanium.modic.backend.common.response.AppResponse;
 import hanium.modic.backend.common.response.PageResponse;
 import hanium.modic.backend.domain.postReview.service.PostReviewCommentService;
@@ -14,10 +27,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/post-review-comments")
@@ -56,7 +65,7 @@ public class PostReviewCommentController {
 	)
 	public ResponseEntity<Void> createComment(
 		@RequestBody @Valid CreatePostReviewCommentRequest request,
-		@AuthenticationPrincipal UserEntity user
+		@CurrentUser UserEntity user
 	) {
 		commentService.createComment(user.getId(), request.postReviewId(), request.text());
 		return ResponseEntity.ok().build();
@@ -74,7 +83,7 @@ public class PostReviewCommentController {
 	public ResponseEntity<Void> updateComment(
 		@PathVariable long commentId,
 		@RequestBody @Valid UpdatePostReviewCommentRequest request,
-		@AuthenticationPrincipal UserEntity user
+		@CurrentUser UserEntity user
 	) {
 		commentService.updateComment(user.getId(), commentId, request.text());
 		return ResponseEntity.ok().build();
@@ -91,7 +100,7 @@ public class PostReviewCommentController {
 	)
 	public ResponseEntity<Void> deleteComment(
 		@PathVariable long commentId,
-		@AuthenticationPrincipal UserEntity user
+		@CurrentUser UserEntity user
 	) {
 		commentService.deleteComment(user.getId(), commentId);
 		return ResponseEntity.ok().build();
