7517a0302e57f9547a815635b6843b4b3ee1e1d0
yooooonshine <hl3470@naver.com>
2025-07-20 17:02:54 +0900

    feat: 유저 회원가입에 토큰추가

--- src/main/java/hanium/modic/backend/web/user/dto/request/UserCreateRequest.java
+++ src/main/java/hanium/modic/backend/web/user/dto/request/UserCreateRequest.java
@@ -10,11 +10,12 @@ public record UserCreateRequest(
 	@Email(message = "이메일 형식으로 요청해주세요.")
 	@NotBlank(message = "이메일은 필수입니다.")
 	String email,
-
 	@NotBlank(message = "이름은 필수입니다.")
 	@Length(min = 1, max = 20, message = "이름은 2자 이상 20자 이하로 입력해주세요.")
 	String name,
 	@Password(message = "비밀번호는 8자 이상 20자 이하, 영문, 숫자, 특수문자를 포함해야 합니다.")
-	String password
+	String password,
+	@NotBlank(message = "인증 코드를 입력해주세요.")
+	String code
 ) {
 }
