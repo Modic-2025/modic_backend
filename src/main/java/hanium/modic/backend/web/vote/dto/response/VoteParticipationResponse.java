64f2e32b60d2f3cc0715eb53229fc1481bbffb73
Dongyoung Kim <goalsetter09@naver.com>
2025-09-07 17:22:43 +0900

    refactor: 투표 기능 응답 단순화

--- src/main/java/hanium/modic/backend/web/vote/dto/response/VoteParticipationResponse.java
+++ src/main/java/hanium/modic/backend/web/vote/dto/response/VoteParticipationResponse.java
@@ -1,31 +1,8 @@
 package hanium.modic.backend.web.vote.dto.response;
 
-import hanium.modic.backend.domain.vote.enums.VoteDecision;
-
-public record VoteParticipationResponse(
-	Long voteId,
-	VoteDecision userDecision,
-	Long currentApproveWeight,
-	Long currentDenyWeight,
-	Long totalWeight,
-	boolean isCompleted
-) {
+public record VoteParticipationResponse(Long voteId) {
 	
-	public static VoteParticipationResponse of(
-		Long voteId,
-		VoteDecision userDecision,
-		Long approveWeight,
-		Long denyWeight,
-		Long totalWeight,
-		boolean isCompleted
-	) {
-		return new VoteParticipationResponse(
-			voteId, 
-			userDecision, 
-			approveWeight, 
-			denyWeight, 
-			totalWeight, 
-			isCompleted
-		);
+	public static VoteParticipationResponse of(Long voteId) {
+		return new VoteParticipationResponse(voteId);
 	}
 }
