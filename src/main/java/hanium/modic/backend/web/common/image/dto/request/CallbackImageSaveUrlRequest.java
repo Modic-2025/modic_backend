2312424f63d59a200b3420524619847daa492e57
yooooonshine <hl3470@naver.com>
2025-09-15 16:00:24 +0900

    fix: Prefix 제거로 인한 오류 수정
    - 해당 Prefix 제거는 다른 branch에서 작업예정

--- src/main/java/hanium/modic/backend/web/common/image/dto/request/CallbackImageSaveUrlRequest.java
+++ src/main/java/hanium/modic/backend/web/common/image/dto/request/CallbackImageSaveUrlRequest.java
@@ -8,6 +8,8 @@ public record CallbackImageSaveUrlRequest(
 	@NotBlank(message = "파일명은 필수입니다.")
 	String fileName,
 	@NotBlank(message = "이미지 Path는 필수입니다.")
-	String imagePath
+	String imagePath,
+	@NotNull(message = "이미지 사용 목적은 필수입니다.")
+	ImagePrefix imageUsagePurpose
 ) {
 }
--- src/main/java/hanium/modic/backend/web/common/image/dto/request/CreateImageSaveUrlRequest.java
+++ src/main/java/hanium/modic/backend/web/common/image/dto/request/CreateImageSaveUrlRequest.java
@@ -6,6 +6,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateImageSaveUrlRequest(
+	@NotNull(message = "이미지 사용 목적은 필수입니다.")
+	@Schema(description = "이미지 사용 목적 (예: PROFILE, AI_REQUEST, AI_RESPONSE, POST)", implementation = ImagePrefix.class, requiredMode = Schema.RequiredMode.REQUIRED)
+	ImagePrefix imageUsagePurpose,
+
 	@NotBlank(message = "파일 이름은 필수입니다.")
 	@Schema(description = "저장할 파일 이름 (확장자 포함)", example = "my_image.png", requiredMode = Schema.RequiredMode.REQUIRED)
 	String fileName
