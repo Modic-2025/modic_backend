c4494bef8ffee82eee768efeb1ae45ae39eac837
yooooonshine <hl3470@naver.com>
2025-10-05 15:06:31 +0900

    refactor: 중복된 textContent 제거

--- src/main/java/hanium/modic/backend/domain/ai/aiServer/listener/AiImageCreatedListener.java
+++ src/main/java/hanium/modic/backend/domain/ai/aiServer/listener/AiImageCreatedListener.java
@@ -135,7 +135,6 @@ public class AiImageCreatedListener {
 			.aiChatRoomId(requestChatMessage.getAiChatRoomId())
 			.messageOrder(messageOrder)
 			.senderType(SenderType.AI)
-			.textContent("")
 			.textContent(message.textContext())
 			.aiChatImageId(null)
 			.requestId(message.requestId())
