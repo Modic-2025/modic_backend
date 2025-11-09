44ed0cadd0e50ac5af539956005cff24c8035399
yooooonshine <hl3470@naver.com>
2025-11-09 23:41:25 +0900

    fix: 인증문제 수정 - 모든 요청을 다 인증을 요구하여 인증이 필요없는 요청도 인증을 요구했다. 이를 수정했다.

--- src/main/java/hanium/modic/backend/common/security/SecurityConfig.java
+++ src/main/java/hanium/modic/backend/common/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
 			.cors(cors -> cors.configurationSource(corsConfigurationSource))
 			.formLogin(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(auth -> auth
-				.anyRequest().authenticated())
+				.anyRequest().permitAll())
 			.sessionManagement(session -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
